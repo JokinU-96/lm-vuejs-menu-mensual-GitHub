a9db18bed7570a279ed52fd438831f8da9256a04
Jokin <jokin.urteaga@ikasle.egibide.org>
2025-03-14 21:50:06 +0100

    Maquetado

---
+++ Dieta/src/components/Navegacion.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="row justify-content-center">
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary">Retroceder</button>
+            <button type="button" class="btn btn-outline-primary">Nueva comida</button>
+            <button type="button" class="btn btn-primary">Avanzar</button>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Dieta/src/stores/Comidas.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+import datos from '@/data/comidas.json';
+
+export const useComidas = defineStore('Comidas', () => {
+
+
+
+    return {  }
+})
